cd07f0691bbbc0e7413af6471dfe91430c751435
rchui <ryan.w.chui@gmail.com>
2017-03-28 14:08:26 -0500

    added basic map reduce classes

--- AllTime.java
+++ AllTime.java
@@ -12,6 +12,20 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AllTime {
+	public static class tokenManager extends Mapper<Object, Text, Text, IntWritable> {
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			
+		}
+	}
+
+	public static class doubleSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
