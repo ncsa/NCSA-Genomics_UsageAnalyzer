ce885056f25a5e3645cb7d4af6bee7943dc2dbe8
rchui <ryan.w.chui@gmail.com>
2017-03-28 14:18:29 -0500

    Word count all words

--- AllTime.java
+++ AllTime.java
@@ -12,21 +12,45 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AllTime {
-	public static class tokenManager extends Mapper<Object, Text, Text, IntWritable> {
-		
+	public static class TokenManager extends Mapper<Object, Text, Text, IntWritable> {
+
+		public static final IntWritable one = new IntWritable(1);
+		public Text word = new Text();
+
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
 		}
 	}
 
-	public static class doubleSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class DoubleSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private IntWritable result = new IntWritable();
 		
 		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			
+			int sum = 0;
+			for (IntWritable val: values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
 		}
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "all time");
+		job.setJarByClass(AllTime.class);
+		job.setMapperClass(TokenManager.class);
+		job.setCombinerClass(DoubleSumReducer.class);
+		job.setReducerClass(DoubleSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
