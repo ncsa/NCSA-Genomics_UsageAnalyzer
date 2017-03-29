7546f9b61d449e9aea44320869a2587dfe41ad6e
rchui <ryan.w.chui@gmail.com>
2017-03-28 19:14:01 -0500

    Fully commented, Final AllTime commit

--- AllTime.java
+++ AllTime.java
@@ -13,12 +13,19 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+// Maps jobs by group and reduces the amount of time taken for each job.
 public class AllTime {
+
+	// Instance to map jobs.
 	public static class TokenManager extends Mapper<Object, Text, Text, DoubleWritable> {
 
 		public DoubleWritable time = new DoubleWritable(1.0);
 		public Text word = new Text();
-
+		
+		// Maps the time taken for a job to their group name.
+		// Parameters: key (Object) unused key object
+		//             value (Text) file text
+		//             context (Context) map/reduce object to interact with Hadoop system
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
 			BufferedReader buff = new BufferedReader(new StringReader(value.toString()));
@@ -37,10 +44,15 @@ public class AllTime {
 		}
 	}
 
+	// Instance to reduce jobs.
 	public static class DoubleSumReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
 		private DoubleWritable result = new DoubleWritable();
 		
+		// Reduces the time taken for a job by their group name.
+		// Paraemters: key (Text) group name
+		//             values (Iterable<DoubleWritable>) times for each job
+		//             context (Context) map/reduce object to interact with Hadoop system
 		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
 			for (DoubleWritable val: values) {
@@ -51,6 +63,8 @@ public class AllTime {
 		}
 	}
 
+	// Creates and configures a new Hadoop job.
+	// Maps by job group name and reduces by time spent on each job.
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "all time");
