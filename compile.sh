cd63140641c3bdfacfe0cd1c50143c72bd9bae52
rchui <ryan.w.chui@gmail.com>
2017-03-30 00:05:15 -0500

    modified compile script for user level hadoop

--- compile.sh
+++ compile.sh
@@ -1,6 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 hadoop com.sun.tools.javac.Main AllTime.java
-hdfs dfs -rmr /user/rchui2/output/
 jar cf at.jar AllTime*.class
-hadoop jar at.jar AllTime /user/rchui2/input/ /user/rchui2/output/
