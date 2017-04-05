17e6e0fe8d5213eaa5427797fb13b24a6e477822
rchui <ryan.w.chui@gmail.com>
2017-04-05 16:44:21 -0500

    moved .java to src and changed compile commands

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hadoop com.sun.tools.javac.Main PBSTime.java
+hadoop com.sun.tools.javac.Main src/PBSTime.java
 jar cf pbs.jar PBSTime*.class
 
-hadoop com.sun.tools.javac.Main OGETime.java
+hadoop com.sun.tools.javac.Main src/OGETime.java
 jar cf oge.jar OGETime*.class
