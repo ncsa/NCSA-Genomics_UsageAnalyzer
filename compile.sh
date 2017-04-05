3db7d654b575f5e48b08db8450c15dbdac61961b
rchui <ryan.w.chui@gmail.com>
2017-04-05 17:13:04 -0500

    changed output directory

--- compile.sh
+++ compile.sh
@@ -5,6 +5,6 @@ rm -rf *.jar
 rm -rf bin/
 
 mkdir bin/
-hadoop com.sun.tools.javac.Main -d bin/ src/*.java
+hadoop com.sun.tools.javac.Main -d bin src/*.java
 jar cf pbs.jar bin/PBSTime*.class
 jar cf oge.jar bin/OGETime*.class
