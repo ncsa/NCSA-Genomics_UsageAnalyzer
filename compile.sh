afda222c34bff01cf1852036e093e9db2f5fe5a5
rchui <ryan.w.chui@gmail.com>
2017-04-07 15:19:42 -0500

    fixed bin jar compile issue

--- compile.sh
+++ compile.sh
@@ -5,5 +5,10 @@ rm -rf bin/
 
 mkdir bin/
 hadoop com.sun.tools.javac.Main -d bin src/*.java
-jar cf pbs.jar bin/PBSTime*.class
-jar cf oge.jar bin/OGETime*.class
+cd bin/
+
+jar cf pbs.jar PBSTime*.class
+jar cf oge.jar OGETime*.class
+
+mv pbs.jar ../
+mv oge.jar ../
